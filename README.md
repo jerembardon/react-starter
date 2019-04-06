15cca35fd49dfeefd67ef17ba31c218f4ed47f78
unknown <jerembardon@gmail.com>
2019-04-06 16:37:53 +0200

    Setting up webpack with React and run simple hello world from app

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# React starter
+React starter
+==================
+
+Configuration
+------------------
+
+This project use webpack & babel 7 for working a simple react app, hot reload and hot module replacement for React are setting up.
+
+Material includes:
+
+*  React 16
+
+Installation
+------------------
+
+```
+> npm install 
+
+> npm run start
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+}
